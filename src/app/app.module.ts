f0c53d550c45b7bacf1c08e800000073f9beca16
Claudio Teles <telesapps85@gmail.com>
2023-04-23 10:14:01 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'chatty-1f77a',
+    appId: '1:29830916830:web:2ac3a69cc5d7f3bf89304f',
+    databaseURL: 'https://chatty-1f77a-default-rtdb.firebaseio.com',
+    storageBucket: 'chatty-1f77a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBJYGWZZ0Tp3N8XI27T80PZmb7FCsDbxdg',
+    authDomain: 'chatty-1f77a.firebaseapp.com',
+    messagingSenderId: '29830916830',
+    measurementId: 'G-QNTDM5CRHE',
+  },};
